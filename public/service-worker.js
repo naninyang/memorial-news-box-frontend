b701ea7e3425b36feb859ae9b9ab1383b4500821
Chloe <gtalk.with.nani@gmail.com>
2024-02-27 14:05:46 +0900

    service worker & sitemap update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,7 @@
 const CACHE_NAME = 'memorial-data';
 
 self.addEventListener('fetch', (event) => {
-  if (
-    event.request.url.includes('/api/articleNews') ||
-    event.request.url.includes('/api/pages') ||
-    event.request.url.includes('/api/editorial') ||
-    event.request.url.includes('/api/watchNews') ||
-    event.request.url.includes('/api/youtubeNews')
-  ) {
+  if (event.request.url.includes('/api/notices')) {
     event.respondWith(
       caches.open(CACHE_NAME).then((cache) => {
         return cache.match(event.request).then((response) => {
